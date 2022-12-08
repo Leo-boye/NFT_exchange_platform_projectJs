3f34421ea6a7bc51f3b5e0873864f9249399b119
Léo Boyé <leo.boye@epita.fr>
2022-12-08 16:08:18 +0100

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NFT exchange platform')
+    .setDescription(
+      'an API used for:\n' +
+        '- User management and authentication\n' +
+        '- Collections management\n' +
+        '- Fake NFT management',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
