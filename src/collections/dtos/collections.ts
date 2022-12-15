f54c79116ef83f71ad90959eb5a33642f0c25dd3
Mathis Aymonin <mathis.aymonin@epita.fr>
2022-12-15 13:50:52 +0100

    feat: set swagger example for collections dto

--- src/collections/dtos/collections.ts
+++ src/collections/dtos/collections.ts
@@ -3,16 +3,16 @@ import { Status } from '@prisma/client';
 import { IsEnum, IsNotEmpty, IsUrl, IsUUID, ValidateIf } from 'class-validator';
 
 export class CollectionCreateDto {
-  @ApiProperty({ example: 'TODO' })
+  @ApiProperty({ example: 'My super collection' })
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty({ example: 'TODO', required: false })
+  @ApiProperty({ example: 'https://example.org', required: false })
   @ValidateIf((object, value) => value !== undefined)
   @IsUrl()
   logo?: string;
 
-  @ApiProperty({ example: 'TODO' })
+  @ApiProperty({ example: 'DRAFT' })
   @IsEnum(Status)
   @IsNotEmpty()
   status: Status;
@@ -21,7 +21,7 @@ export class CollectionCreateDto {
 export class CollectionUpdateDto extends PartialType(CollectionCreateDto) {}
 
 export class CollectionDto extends CollectionCreateDto {
-  @ApiProperty({ example: 'TODO' })
+  @ApiProperty({ example: '59c78745-aa9e-4930-b338-214aff8b07be' })
   @IsUUID()
   @IsNotEmpty()
   id: string;
