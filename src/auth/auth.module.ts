d86bb690175304abb0f445efe7a7449583565d3f
Mathis Aymonin <mathis.aymonin@epita.fr>
2022-12-16 12:35:18 +0100

    feat: change JWT expiration duration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
